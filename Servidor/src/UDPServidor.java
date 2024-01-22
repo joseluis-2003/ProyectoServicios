8aa03343f93a507f9e6189a4c87808805289dc05
rikialamo <rikialamo@gmail.com>
2024-01-22 19:41:55 +0100

    arreglo error package

--- Servidor/src/UDPServidor.java
+++ Servidor/src/UDPServidor.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import com.example.cliente.Interfaces.ConstantsInterface;
+import Interfaces.ConstantsInterface;
 
 public class UDPServidor implements ConstantsInterface {
 

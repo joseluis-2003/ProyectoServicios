d87bf320ccd69e9a500bc4b49a818cca35aa2e17
rikialamo <rikialamo@gmail.com>
2024-01-22 20:01:32 +0100

    clase cliente

---
+++ Cliente/src/main/java/com/example/cliente/Cliente.java
@@ -0,0 +1,48 @@
+package com.example.cliente;
+
+import java.net.InetAddress;
+
+public class Cliente {
+
+    private String apodo;
+    private InetAddress inetAddress;
+
+    public Cliente(String apodo, InetAddress inetAddress) {
+        this.apodo = apodo;
+        this.inetAddress = inetAddress;
+    }
+
+    public Cliente(InetAddress inetAddress) {
+        this.inetAddress = inetAddress;
+        apodo = null;
+    }
+
+    public String getApodo() {
+        if(apodoValidator()) {
+            return apodo;
+        }
+        else{
+            //throw error
+            return "";
+        }
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    public InetAddress getInetAddress() {
+        return inetAddress;
+    }
+
+    public void setInetAddress(InetAddress inetAddress) {
+        this.inetAddress = inetAddress;
+    }
+
+    private boolean apodoValidator(){
+        if(apodo == null){
+            return false;
+        }
+        return true;
+    }
+}

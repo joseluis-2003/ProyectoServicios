d400907d56d584f3edcdfa0572bcac6bf6e4a3ac
Ricardo <rikialamo@gmail.com>
2024-01-22 22:55:42 +0100

    JavaDoc Servidor

--- Servidor/src/Main.java
+++ Servidor/src/Main.java
@@ -1,8 +1,19 @@
 import server.UDPServidor;
 
+/**
+ * La clase Main contiene el método principal que inicia el servidor UDP.
+ */
 public class Main {
+
+    /**
+     * El método principal de la aplicación que inicia el servidor UDP.
+     *
+     * @param args Los argumentos de la línea de comandos (no se utilizan en este caso).
+     */
     public static void main(String[] args) {
+        // Crear una instancia de UDPServidor y iniciar el servidor.
         UDPServidor server = new UDPServidor();
         server.iniciarServidor();
     }
 }
+
--- Servidor/src/server/UDPServidor.java
+++ Servidor/src/server/UDPServidor.java
@@ -2,18 +2,35 @@ package server;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.*;
-
 import Interfaces.ConstantsInterface;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-public class UDPServidor extends Cliente implements ConstantsInterface{
+/**
+ * La clase UDPServidor extiende la clase Cliente e implementa la interfaz ConstantsInterface,
+ * proporcionando funcionalidad para gestionar un servidor UDP y la comunicación con clientes.
+ */
+public class UDPServidor extends Cliente implements ConstantsInterface {
 
+    /**
+     * Lista que almacena objetos Cliente que representan a los clientes conectados al servidor.
+     */
     private List<Cliente> clientes;
 
+    /**
+     * Constructor predeterminado de la clase UDPServidor.
+     * Inicializa la lista de clientes.
+     */
     public UDPServidor() {
         clientes = new ArrayList<>();
     }
 
+    /**
+     * Inicia el servidor UDP y espera la conexión de clientes.
+     * Este método se ejecuta en un bucle infinito, recibiendo y procesando mensajes de los clientes.
+     * El servidor puede ser detenido enviando un mensaje "STOP".
+     */
     public void iniciarServidor() {
         try (DatagramSocket socketServidor = new DatagramSocket(PUERTO_SERVIDOR)) {
             System.out.println("El servidor está en ejecución. Esperando clientes...");
@@ -40,19 +57,34 @@ public class UDPServidor extends Cliente implements ConstantsInterface{
         }
     }
 
+    /**
+     * Maneja el registro de apodo de un cliente.
+     * Extrae el apodo del mensaje y verifica su disponibilidad antes de asignarlo al cliente.
+     *
+     * @param mensaje           El mensaje que contiene el apodo a registrar.
+     * @param direccionCliente La dirección IP del cliente que envía el mensaje.
+     */
     private void manejarRegistroApodo(String mensaje, InetAddress direccionCliente) {
         String apodo = mensaje.substring(5);
         if (verificarApodo(apodo)) {
             Cliente cliente = buscarCliente(direccionCliente);
             if(cliente != null){
                 cliente.setApodo(apodo);
+                System.out.println("server.Cliente " + direccionCliente + " registrado con apodo: " + apodo);
+            }else{
+                System.out.println("Cliente no encontrado");
             }
-            System.out.println("server.Cliente " + direccionCliente + " registrado con apodo: " + apodo);
         } else {
             System.out.println("Apodo ya en uso. Por favor, elige otro.");
         }
     }
 
+    /**
+     * Reenvía un mensaje a todos los clientes excepto al remitente.
+     *
+     * @param mensaje            El mensaje a reenviar.
+     * @param direccionRemitente La dirección IP del cliente que envió el mensaje original.
+     */
     private void reenviarMensajeAClientes(String mensaje, InetAddress direccionRemitente) {
         try (DatagramSocket socketCliente = new DatagramSocket()) {
             byte[] datosEnviar = mensaje.getBytes();
@@ -61,7 +93,7 @@ public class UDPServidor extends Cliente implements ConstantsInterface{
 
             while (iterador.hasNext()) {
                 Cliente cliente = iterador.next();
-                if (cliente.getInetAddress()==direccionRemitente) {
+                if (cliente.getInetAddress() == direccionRemitente) {
                     DatagramPacket paqueteEnviar = new DatagramPacket(datosEnviar, datosEnviar.length,
                             cliente.getInetAddress(), PUERTO_CLIENTE);
                     socketCliente.send(paqueteEnviar);
@@ -72,12 +104,18 @@ public class UDPServidor extends Cliente implements ConstantsInterface{
         }
     }
 
-    private boolean verificarApodo(String apodo){
+    /**
+     * Verifica si un apodo ya está en uso por otro cliente.
+     *
+     * @param apodo El apodo a verificar.
+     * @return true si el apodo está disponible; false, de lo contrario.
+     */
+    private boolean verificarApodo(String apodo) {
         Iterator<Cliente> iterador = clientes.iterator();
 
         while (iterador.hasNext()) {
             Cliente cliente = iterador.next();
-            if(cliente.getApodo().equals(apodo)){
+            if (cliente.getApodo().equals(apodo)) {
                 return false;
             }
         }
@@ -85,12 +123,18 @@ public class UDPServidor extends Cliente implements ConstantsInterface{
         return true;
     }
 
-    private Cliente buscarCliente(InetAddress inetAddress){
+    /**
+     * Busca un cliente en la lista de clientes basándose en su dirección IP.
+     *
+     * @param inetAddress La dirección IP del cliente a buscar.
+     * @return El objeto Cliente correspondiente a la dirección IP dada, o null si no se encuentra.
+     */
+    private Cliente buscarCliente(InetAddress inetAddress) {
         Iterator<Cliente> iterador = clientes.iterator();
 
         while (iterador.hasNext()) {
             Cliente cliente = iterador.next();
-            if(cliente.getInetAddress() == inetAddress){
+            if (cliente.getInetAddress() == inetAddress) {
                 return cliente;
             }
         }
